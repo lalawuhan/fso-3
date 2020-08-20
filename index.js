f202205a3f0313f0edb7d1836fafdd0fffcf1edb
Rethabile Segoe <precious.segoe@gmail.com>
2020-08-20 17:03:12 +0200

    in progress with ex3.9

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 morgan.token("body", function (req, res) {
@@ -117,7 +119,7 @@ app.post("/api/persons", (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
