91994b34fe10b4cac503143515cf56c63638ff38
Rethabile Segoe <precious.segoe@gmail.com>
2020-08-20 18:10:04 +0200

    finish ex 3.10-3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
